06a7eb103c41241cd7f65df1f43e82d08d2000bb
anoopraju31 <anoopraju31@gmail.com>
2024-07-20 20:59:35 +0530

    Game component - initial

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import { type FC } from 'react'
+import Game from './_components/Game'
 
 const HomePage: FC = () => {
-	return <div>HomePage</div>
+	return (
+		<main className='min-h-screen bg-black text-white'>
+			<Game />
+		</main>
+	)
 }
 
 export default HomePage
